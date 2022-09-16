da3e693dcdf25d4fb5f844e04bee3d68d1281f4e
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2022-09-16 18:43:50 +0200

    secrurity packages

--- job-API/starter/app.js
+++ job-API/starter/app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security package
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+ 
+
 const express = require('express');
 const app = express();
 
@@ -15,8 +23,27 @@ const jobRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// configure proxy for cors policy 
+app.set('trust proxy', 1 );
+
+
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+// limiter les requêtes réalisée par un utilisateur sur une période données pour éviter les tentatives de hacker
+app.use(rateLimiter({
+  windowMS: 15*60*100,
+  max: 100, 
+}))
+
+// deployement part
+/* 
+app.get('/', (req, res) => {
+    res.send('job api')
+})
+*/
 
 // routes
 app.use('/api/v1/auth', authRouter);
