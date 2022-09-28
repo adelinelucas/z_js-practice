2019020fb0d3d376f5b1d0e57e53da416a072170
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2022-09-28 20:32:13 +0200

    fetch api & react router

---
+++ react-cocktailBlog/setup/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis fuga iure earum. Ullam, nulla iure recusandae facilis rerum deleniti, illo dolorum pariatur repellat sit enim?
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi quibusdam ad soluta animi nostrum quos quae dolores nobis! Rerum in explicabo quo error, id consequuntur officia! Fuga possimus, est facilis incidunt debitis voluptates officiis delectus accusantium saepe quod obcaecati tempora?</p>
+    </section>
+  )
+}
+
+export default About
