329f9647e1048777675f2306eb4aba974e097e5d
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-01-31 23:57:55 +0100

    FCC-Portfolio

---
+++ react-FCC-projectCourse/ToolTip_reactPackages.md
@@ -0,0 +1,30 @@
+### React Package installed 
+
+## Style
+sass
+
+## Router 
+react-router-dom
+
+## Email
+@emailjs/browser
+
+## Icons
+@fortawesome/free-brands-svg-icons 
+@fortawesome/free-solid-svg-icons
+@fortawesome/react-fontawesome
+
+react-icons
+
+## Map
+react-leaflet
+
+## Animations 
+animate.css
+
+# Loaders
+loaders.css
+react-loaders
+
+# GreenSock Animation Platform
+gsap-trial
---
+++ react-FCC-projectCourse/react_portfolio/src/components/Home/index.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+import './index.scss';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray=['A','d','e','l','i','n','e'];
+    const jobArray=['w','e','b',' ','d','e','v','e','l','o','p','e','r', ' ','!'];
+
+    useEffect(() => {
+        setTimeout(() => {
+          return setLetterClass('text-animate-hover')
+        }, 4000)
+      }, [])
+
+    return (
+        <div className='container home-page' >
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`s{letterClass} _12`}>i,</span> 
+                    <br/>
+                    <span className={`s{letterClass} _13`}>I</span> 
+                    <span className={`s{letterClass} _14`}>'m</span> 
+                    <img src={LogoTitle} alt="developper" />
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+                    <br />
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}/>
+
+                </h1>
+                <h2>Frontend Developer / Javascript / Youtuber</h2>
+                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo />
+        </div>
+    );
+};
+
+export default Home;
